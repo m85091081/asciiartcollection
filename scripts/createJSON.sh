8c8ba2c8756dec92d8635738259435e03925f5d4
dd-han <han@dd-han.tw>
2015-11-01 08:58:51 +0800

    update createJSON.sh

--- scripts/createJSON.sh
+++ scripts/createJSON.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
-OUT_TXT="list-txt.json"
-OUT_PNG="list-png.json"
+## 以下路徑都是相對於本Scipt的 ** 工作目錄的 **
+
+# 設定txt與png存放路徑
+MEDIA_FOLD="../media"
+# 設定JSON檔存放的路徑
+OUT_JSON_FOLD="../json/"
+# 輸出的包含主列表的JSON檔名
+OUT_JSON="list.json"
 
 header="";
+script_locate="`pwd`";
 
+cd ${MEDIA_FOLD}
 for file in `ls *.txt`;do
 	if [ "$header" == "" ]; then
-		rm $OUT_TXT
-		rm $OUT_PNG
+		rm "${script_locate}/${OUT_JSON_FOLD}/${OUT_JSON}"
 	fi
-
-	echo -n ${header}'{'${file}'}' >> $OUT_TXT
 	
-	pngFile=`echo ${file} | sed s/\.txt/.png/g`
-	echo -n ${header}'{'${pngFile}'}' >> $OUT_PNG
+	mainFileName=`echo ${file} | sed s/\.txt//g`
+	echo -n ${header}'{'$mainFileName}'}' >> "${script_locate}/${OUT_JSON_FOLD}/${OUT_JSON}"
 
 	header=","
 done
-
